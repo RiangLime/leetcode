acef3842647969a8356db440c4337f221d7e1b95
jiaqi <jiaqi@queertech.cn>
2024-04-26 09:54:31 +0800

    commit ans

---
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q0104.java
@@ -0,0 +1,96 @@
+//给定一个二叉树 root ，返回其最大深度。 
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 10⁴] 区间内。 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1819 👎 0
+
+
+package cn.lime.leetcode.leetcode.editor.cn;
+
+import cn.lime.leetcode.leetcode.editor.cn.comm.TreeNode;
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Q0104 {
+    public static void main(String[] args) {
+        Solution solution = new Q0104().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null){
+                return 0;
+            }
+
+            Queue<TreeNode> queue = new ArrayDeque<>();
+            queue.offer(root);
+            int res = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                res++;
+            }
+            return res;
+
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
