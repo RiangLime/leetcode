579bfec506b4d6fca02bee3e331a9732d2bf5abf
jiaqi <jiaqi@queertech.cn>
2024-04-30 10:45:42 +0800

    commit answer

--- src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q0541.java
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q0541.java
@@ -39,62 +39,67 @@ package cn.lime.leetcode.leetcode.editor.cn;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Q0541{
+public class Q0541 {
     public static void main(String[] args) {
-         Solution solution = new Q0541().new Solution();
-         solution.reverseStr("abcdefg",2);
-    } 
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String reverseStr(String s, int k) {
-        StringBuilder tmp = new StringBuilder();
-        StringBuilder res = new StringBuilder();
-        List<String> splitList = new ArrayList<>();
-        char[] cArr = s.toCharArray();
-        for (int i = 0; i < cArr.length; i++) {
-
-            tmp.append(cArr[i]);
-            if (i%(2*k)==((2*k)-1)){
-                splitList.add(tmp.toString());
-                tmp = new StringBuilder();
-            }
-            if (i==cArr.length-1 && tmp.length()!=0){
-                splitList.add(tmp.toString());
-
-            }
-        }
-
-        for (int i = 0; i < splitList.size(); i++) {
-            char[] cs = splitList.get(i).toCharArray();
-            if (i!= splitList.size()-1){
-
-                for (int j = k-1; j >=0; j--) {
-                    res.append(cs[j]);
-                }
-                for (int j = k; j < 2 * k; j++) {
-                    res.append(cs[j]);
-                }
-            }else {
-                if (cs.length<k){
-                    for (int j = cs.length-1; j >=0; j--) {
-                        res.append(cs[j]);
-                    }
-                }else {
-                    for (int j = k-1; j >=0; j--) {
-                        res.append(cs[j]);
-                    }
-                    for (int j = k; j < cs.length; j++) {
-                        res.append(cs[j]);
-                    }
-                }
-            }
-        }
-
-        return res.toString();
-
-
+        Solution solution = new Q0541().new Solution();
+        solution.reverseStr("abcdefg", 2);
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String reverseStr(String s, int k) {
+            return null;
+        }
+
+        public String reverseStrViolent(String s, int k) {
+            StringBuilder tmp = new StringBuilder();
+            StringBuilder res = new StringBuilder();
+            List<String> splitList = new ArrayList<>();
+            char[] cArr = s.toCharArray();
+            for (int i = 0; i < cArr.length; i++) {
+
+                tmp.append(cArr[i]);
+                if (i % (2 * k) == ((2 * k) - 1)) {
+                    splitList.add(tmp.toString());
+                    tmp = new StringBuilder();
+                }
+                if (i == cArr.length - 1 && tmp.length() != 0) {
+                    splitList.add(tmp.toString());
+
+                }
+            }
+
+            for (int i = 0; i < splitList.size(); i++) {
+                char[] cs = splitList.get(i).toCharArray();
+                if (i != splitList.size() - 1) {
+
+                    for (int j = k - 1; j >= 0; j--) {
+                        res.append(cs[j]);
+                    }
+                    for (int j = k; j < 2 * k; j++) {
+                        res.append(cs[j]);
+                    }
+                } else {
+                    if (cs.length < k) {
+                        for (int j = cs.length - 1; j >= 0; j--) {
+                            res.append(cs[j]);
+                        }
+                    } else {
+                        for (int j = k - 1; j >= 0; j--) {
+                            res.append(cs[j]);
+                        }
+                        for (int j = k; j < cs.length; j++) {
+                            res.append(cs[j]);
+                        }
+                    }
+                }
+            }
+
+            return res.toString();
+
+
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
