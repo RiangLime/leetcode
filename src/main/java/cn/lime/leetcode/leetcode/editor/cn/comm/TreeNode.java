c110447a4a5f492175b5135812cb4e3f3c0489e9
jiaqi <jiaqi@queertech.cn>
2024-04-30 14:51:44 +0800

    commit answer

--- src/main/java/cn/lime/leetcode/leetcode/editor/cn/comm/TreeNode.java
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/comm/TreeNode.java
@@ -11,6 +11,12 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
     public TreeNode(int x) { val = x; }
 
     public int getVal() {
