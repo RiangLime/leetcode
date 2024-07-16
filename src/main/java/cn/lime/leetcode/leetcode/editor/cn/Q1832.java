61c6baefb85482634ebec3af0e0c7e4e238fdb76
jiaqi <jiaqi@queertech.cn>
2024-07-16 15:05:24 +0800

    commit answer

---
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q1832.java
@@ -0,0 +1,57 @@
+//全字母句 指包含英语字母表中每个字母至少一次的句子。 
+//
+// 给你一个仅由小写英文字母组成的字符串 sentence ，请你判断 sentence 是否为 全字母句 。 
+//
+// 如果是，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：sentence = "thequickbrownfoxjumpsoverthelazydog"
+//输出：true
+//解释：sentence 包含英语字母表中每个字母至少一次。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：sentence = "leetcode"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= sentence.length <= 1000 
+// sentence 由小写英语字母组成 
+// 
+//
+// Related Topics 哈希表 字符串 👍 81 👎 0
+
+
+package cn.lime.leetcode.leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q1832{
+    public static void main(String[] args) {
+         Solution solution = new Q1832().new Solution();
+    } 
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+        for (char c : sentence.toCharArray()) {
+            set.add(c);
+        }
+        return set.size()==24;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
