5701b164cabd4943c033cc617450226943b13e36
jiaqi <jiaqi@queertech.cn>
2024-07-16 17:25:03 +0800

    commit answer

---
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q1909.java
@@ -0,0 +1,89 @@
+//给你一个下标从 0 开始的整数数组 nums ，如果 恰好 删除 一个 元素后，数组 严格递增 ，那么请你返回 true ，否则返回 false 。如果数组
+//本身已经是严格递增的，请你也返回 true 。 
+//
+// 数组 nums 是 严格递增 的定义为：对于任意下标的 1 <= i < nums.length 都满足 nums[i - 1] < nums[i] 。 
+//
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [1,2,10,5,7]
+//输出：true
+//解释：从 nums 中删除下标 2 处的 10 ，得到 [1,2,5,7] 。
+//[1,2,5,7] 是严格递增的，所以返回 true 。
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [2,3,1,2]
+//输出：false
+//解释：
+//[3,1,2] 是删除下标 0 处元素后得到的结果。
+//[2,1,2] 是删除下标 1 处元素后得到的结果。
+//[2,3,2] 是删除下标 2 处元素后得到的结果。
+//[2,3,1] 是删除下标 3 处元素后得到的结果。
+//没有任何结果数组是严格递增的，所以返回 false 。 
+//
+// 示例 3： 
+//
+// 输入：nums = [1,1,1]
+//输出：false
+//解释：删除任意元素后的结果都是 [1,1] 。
+//[1,1] 不是严格递增的，所以返回 false 。
+// 
+//
+// 示例 4： 
+//
+// 输入：nums = [1,2,3]
+//输出：true
+//解释：[1,2,3] 已经是严格递增的，所以返回 true 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 1000 
+// 1 <= nums[i] <= 1000 
+// 
+//
+// Related Topics 数组 👍 49 👎 0
+
+
+package cn.lime.leetcode.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q1909 {
+    public static void main(String[] args) {
+        Solution solution = new Q1909().new Solution();
+        solution.canBeIncreasing(new int[]{2,3,1,2});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean canBeIncreasing(int[] nums) {
+            List<Integer> positions = new ArrayList<>();
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] <= nums[i - 1]) {
+                    positions.add(i - 1);
+                }
+            }
+            if (positions.size() > 1) return false;
+            if (positions.size() == 0) return true;
+            int p = positions.get(0);
+            // 驼峰
+            if (p-1 <0 || nums[p-1] < nums[p+1]) return true;
+            // 低估
+            if (p+2 > nums.length-1 || nums[p+2]>nums[p]) return true;
+            return false;
+
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
