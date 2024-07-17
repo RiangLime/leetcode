aa8181698868a842f11fe5bc9bd6c5da0e6eb844
jiaqi <jiaqi@queertech.cn>
2024-07-17 14:32:02 +0800

    commit answer

---
+++ src/main/java/cn/lime/leetcode/leetcode/editor/cn/Q1979.java
@@ -0,0 +1,75 @@
+//给你一个整数数组 nums ，返回数组中最大数和最小数的 最大公约数 。 
+//
+// 两个数的 最大公约数 是能够被两个数整除的最大正整数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [2,5,6,9,10]
+//输出：2
+//解释：
+//nums 中最小的数是 2
+//nums 中最大的数是 10
+//2 和 10 的最大公约数是 2
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [7,5,6,8,3]
+//输出：1
+//解释：
+//nums 中最小的数是 3
+//nums 中最大的数是 8
+//3 和 8 的最大公约数是 1
+// 
+//
+// 示例 3： 
+//
+// 输入：nums = [3,3]
+//输出：3
+//解释：
+//nums 中最小的数是 3
+//nums 中最大的数是 3
+//3 和 3 的最大公约数是 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 1000 
+// 1 <= nums[i] <= 1000 
+// 
+//
+// Related Topics 数组 数学 数论 👍 48 👎 0
+
+
+package cn.lime.leetcode.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class Q1979 {
+    public static void main(String[] args) {
+        Solution solution = new Q1979().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findGCD(int[] nums) {
+            Arrays.sort(nums);
+            int min = nums[0];
+            int max = nums[nums.length - 1];
+
+            for (int i = min; i > 0; i--) {
+                if (min % i == 0 && max % i == 0) return i;
+            }
+            return 1;
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
